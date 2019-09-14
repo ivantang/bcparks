adeefc69935563b866b2630d15c259f6d84f69eb
Peter Ghanem <ens80131@adobe.com>
2019-09-14 13:02:13 -0700

    added progress bar

---
+++ components/ProgressBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ProgressBar, Colors } from 'react-native-paper';
+
+const progressBarComponent = () => (
+    <ProgressBar progress={0.5} color={Colors.red800} />
+);
+
+export default progressBarComponent;
