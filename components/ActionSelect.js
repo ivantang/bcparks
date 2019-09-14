d5f0811bff60f86f7f128b4ea5be870988ca78a3
Peter Ghanem <ens80131@adobe.com>
2019-09-14 15:24:27 -0700

    action select initial

---
+++ components/ActionSelect.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export function ActionSelect(props){
+    return (
+        <View style={{flex: 4, justifyContent: "flex-end", alignItems: "stretch"}}>
+            <View style={{height: 400, backgroundColor: 'steelblue'}} />
+        </View>
+    );
+}
