de89e1aaed6bb361cf1da84b8a6021cfdd397d9f
Peter Ghanem <ens80131@adobe.com>
2019-09-14 20:16:47 -0700

    added more stuf

--- screens/FormScreen.js
+++ screens/FormScreen.js
@@ -1,20 +1,39 @@
-import Form from 'react-native-form';
-import {ScrollView, TextInput, View, Switch, Slider, DatePickerIOS, Picker, PickerIOS, StyleSheet} from "react-native";
-import React from "react";
+import {ScrollView, TextInput, View, KeyboardAvoidingView, Text} from "react-native";
+import React, { Component } from 'react';
 
 export default function FormScreen() {
+    const [parkValue, onChangePark] = React.useState('Juniper Beach Provincial Park');
+    const [descValue, onChangeDesc] = React.useState('Briefly Describe The Issue');
+    const [volunteerValue, onChangeVolunteers] = React.useState('');
+
     return (
-        <Form ref="form">
-            <View>
-                <View>
-                    <TextInput type="TextInput" name="myTextInput" /> // Yes, it doesn't matter how deep they are :)
-                </View>
-            </View>
-            <Switch type="Switch" name="mySwitch" />
-            <Slider type="Slider" name="mySlider" />
-            <DatePickerIOS type="DatePickerIOS" name="myBirthday" />
-            <Picker type="Picker" name="myPicker" />
-            <PickerIOS type="PickerIOS" name="pickers[ios]" /> // Yes, we support form serialization, like the web
-        </Form>
+        <KeyboardAvoidingView
+            behavior="padding">
+                <Text style={{margin: 25}}/>
+                <Text style={{marginHorizontal: 10, marginBottom: 6, color: "#ACACAC"}}>LOCATION</Text>
+                <TextInput
+                    style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginHorizontal: 10 }}
+                    onChangePark={text => onChangePark(text)}
+                    parkValue={parkValue}
+                />
+                <Text style={{margin: 15}}/>
+                <Text style={{marginHorizontal: 10, marginBottom: 6, color: "#ACACAC"}}>ISSUE</Text>
+                <TextInput
+                    multiline
+                    numberOfLines={4}
+                    style={{ height: 100, borderColor: 'gray', borderWidth: 1, marginHorizontal: 10 }}
+                    onChangeDesc={text => onChangeDesc(text)}
+                    descValue={descValue}
+                />
+                <Text style={{margin: 15}}/>
+                <Text style={{marginHorizontal: 10, marginBottom: 6, color: "#ACACAC"}}>NUMBER OF VOLUNTEERS</Text>
+                <TextInput
+                    style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginHorizontal: 10 }}
+                    onChangeVolunteers={text => onChangeVolunteers(text)}
+                    volunteerValue={volunteerValue}
+                />
+            <View style={{ height: 60 }} />
+        </KeyboardAvoidingView>
+
     );
 }
