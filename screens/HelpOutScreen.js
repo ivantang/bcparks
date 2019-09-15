78b38ee0fb9262a0a0fb8b80e4af0ca23f95e3de
Ivan Tang <ivant@ensemble.com>
2019-09-15 12:16:15 -0700

    merge header component

--- screens/HelpOutScreen.js
+++ screens/HelpOutScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, View , StyleSheet} from 'react-native';
 import ActionButton from '../components/ActionButton';
+import { HelpOutHeader } from '../components/Header';
 
 import HelpOutIcon from '../assets/images/Help.png';
 import ReportIssueIcon from '../assets/images/Issue.png';
@@ -8,23 +9,26 @@ import ReportIssueIcon from '../assets/images/Issue.png';
 const styles = StyleSheet.create({
   actionButtonStyle: {
     width: 300,
-    marginTop: 50,
+    marginTop: 10,
   },
 })
 
 export default function HelpOutScreen() {
   return (
-    <View style={{ alignItems: 'center', }}>
-      <View style={styles.actionButtonStyle}>
-        <ActionButton action="Help Out" imageSource={HelpOutIcon} color="#3EC1F0" />
-      </View>
-      <View style={styles.actionButtonStyle}>
-        <ActionButton
-          action="Report an Issue"
-          imageSource={ReportIssueIcon}
-          color="#A7C62E"
-          textColor="white"
-        />
+    <View>
+      <HelpOutHeader />
+      <View style={{ alignItems: 'center', }}>
+        <View style={styles.actionButtonStyle}>
+          <ActionButton action="Help Out" imageSource={HelpOutIcon} color="#3EC1F0" />
+        </View>
+        <View style={styles.actionButtonStyle}>
+          <ActionButton
+            action="Report an Issue"
+            imageSource={ReportIssueIcon}
+            color="#A7C62E"
+            textColor="white"
+          />
+        </View>
       </View>
     </View>
   );
