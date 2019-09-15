75b3495d50cd7935f18b71cb7bb198e2c80a3012
Ivan Tang <ivant@ensemble.com>
2019-09-14 19:55:49 -0700

    add more screens

---
+++ screens/ReportIssueScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function ReportIssueScreen() {
+  return (
+    <View>
+      <Text>Welcome to ReportIssueScreen</Text>
+    </View>
+  );
+}
+
+ReportIssueScreen.navigationOptions = {
+  title: 'app.json',
+};
